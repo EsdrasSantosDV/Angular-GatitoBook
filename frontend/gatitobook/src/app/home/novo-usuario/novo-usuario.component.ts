4ed8c95fb2d646485cb0303afd5f18b4c451b9b2
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-08-29 18:10:30 -0300

    Validaçoes Customizadas

--- frontend/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ frontend/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,4 +1,8 @@
+import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NovoUsuario } from './novo-usuario';
+import { minusculoValidator } from './minusculo.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./novo-usuario.component.css']
 })
 export class NovoUsuarioComponent implements OnInit {
+  //! PARA INICILIZAR AO INVES DO CONTRUTOR NO NG ONINIT
+  novoUsuarioForm!:FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder:FormBuilder,
+    private novoUsuarioService:NovoUsuarioService
+    ) { }
 
+  //REQUIRED OBRIGATORIO,EMAIL
   ngOnInit(): void {
+    this.novoUsuarioForm=this.formBuilder.group({
+      email:['', [Validators.required,Validators.email]],
+      fullName:['',[Validators.required,Validators.minLength(4)]],
+      userName:['',[minusculoValidator]],
+      password:[''],
+
+
+    });
+
+  }
+
+  cadastrar(){
+    const NovoUsuario=this.novoUsuarioForm.getRawValue() as NovoUsuario;
+    console.log(NovoUsuario)
   }
 
 }
